8d18265619b2dc32d89b8c979100ecbee2464ffd
Aryeh Braid David <aryeh.david@e-unicred.com.br>
2023-01-09 09:55:47 -0300

    feat: added build files to .gitignore

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,57 +5,36 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
+import { TypographyProps } from "./components/app-typography/app-typography";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
+    interface AppTypography {
+        "variant"?: TypographyProps;
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    interface HTMLAppTypographyElement extends Components.AppTypography, HTMLStencilElement {
     }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
+    var HTMLAppTypographyElement: {
+        prototype: HTMLAppTypographyElement;
+        new (): HTMLAppTypographyElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
+        "app-typography": HTMLAppTypographyElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
+    interface AppTypography {
+        "variant"?: TypographyProps;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
+        "app-typography": AppTypography;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "app-typography": LocalJSX.AppTypography & JSXBase.HTMLAttributes<HTMLAppTypographyElement>;
         }
     }
 }
