5139f58b50f61d688b536ba0a21d3d0218a01aa5
Aryeh Braid David <aryeh.david@e-unicred.com.br>
2023-01-09 10:11:36 -0300

    feat: added support for material design

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,13 +1,43 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
+const path = require('path');
+
+function tryResolve_(url, sourceFilename) {
+  // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
+  // when the importer throws
+  try {
+    return require.resolve(url, {paths: [path.dirname(sourceFilename)]});
+  } catch (e) {
+    return '';
+  }
+}
+
+function tryResolveScss(url, sourceFilename) {
+  // Support omission of .scss and leading _
+  const normalizedUrl = url.endsWith('.scss') ? url : `${url}.scss`;
+  return tryResolve_(normalizedUrl, sourceFilename) ||
+    tryResolve_(path.join(path.dirname(normalizedUrl), `_${path.basename(normalizedUrl)}`),
+      sourceFilename);
+}
+
+function materialImporter(url, prev) {
+  if (url.startsWith('@material')) {
+    const resolved = tryResolveScss(url, prev);
+    return {file: resolved || url};
+  }
+  return {file: url};
+}
+
 export const config: Config = {
   namespace: 'web-components-library',
   globalStyle: 'src/global/app.scss',
   globalScript: 'src/global/app.ts',
-  plugins: [
-    sass()
-  ],
+  taskQueue: 'async',
+  plugins: [sass({
+    importer: materialImporter,
+    includePaths: ['./node_modules']
+  })],
   outputTargets: [
     {
       type: 'dist',
